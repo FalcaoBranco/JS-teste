d86ab4894032955f136d8a23da2e3fcd70ea2a91
FalcãoBranco51 <106679750+FalcaoBranco@users.noreply.github.com>
2024-07-03 11:07:39 -0300

    tarefa concluida

--- exercicios.js
+++ exercicios.js
@@ -8,8 +8,9 @@
  */
 function soma(a, b) {
   // @todo implementar retorno da operação aqui
+  return (a + b) 
 }
-
+soma(10, 8) 
 /**
  * Função que calcula a multiplicação de 2 números
  * @param {number} a - O primeiro número.
@@ -18,7 +19,9 @@ function soma(a, b) {
  */
 function multiplicacao(a, b) {
   // @todo implementar retorno da operação aqui
+  return (a * b)
 }
+multiplicacao(5,8)
 
 /**
  * Função que calcula a divisão de 2 números
@@ -28,7 +31,9 @@ function multiplicacao(a, b) {
  */
 function divisao(a, b) {
   // @todo implementar retorno da operação aqui
+  return (a / b)
 }
+divisao(10,2)
 
 /**
  * Função que calcula a subtração de 2 números
@@ -38,8 +43,9 @@ function divisao(a, b) {
  */
 function subtracao(a, b) {
   // @todo implementar retorno da operação aqui
+  return (a - b)
 }
-
+subtracao(7,5)
 /**
  * Função que resto da divisão de 2 números
  * @param {number} a - O primeiro número.
@@ -48,8 +54,9 @@ function subtracao(a, b) {
  */
 function modulo(a, b) {
   // @todo implementar retorno da operação aqui
+  return (a % b)
 }
-
+modulo(25,5)
 /**
  * Função que calcula o maior número entre 2
  * @param {number} a - O primeiro número.
@@ -58,8 +65,11 @@ function modulo(a, b) {
  */
 function max(a, b) {
   // @todo implementar retorno da operação aqui
+  if (a > b) return (a)
+  else return(b)
 }
-
+max(15,10)
+max(10,15)
 // -------------------------------------
 
 /**
@@ -70,8 +80,9 @@ function max(a, b) {
  */
 function strConcat(a, b) {
   // @todo implementar retorno da operação aqui
+  return `${a} ${b}`
 }
-
+strConcat("João Victor","Falcão Ribeiro")
 // --------------------------------------
 
 /**
@@ -85,8 +96,19 @@ function strConcat(a, b) {
  */
 function retornaNumero(numero) {
   // @todo implementar retorno da operação aqui
+  if (numero > 10){
+    return "Maior que 10"
+  }
+  else if (numero === 10){
+    return "Igual a 10"
+  }
+  else {
+    return "Menor que 10"
+  }
 }
-
+retornaNumero(11)
+retornaNumero(10)
+retornaNumero(9)
 // --------------------------------------
 
 /**
@@ -96,9 +118,18 @@ function retornaNumero(numero) {
  */
 function ehPositivo(num) {
   // @todo implementar retorno da operação aqui
-}
+  if (num >= 0){
+    return (true)
+  }
+  else {
+    return false
+  }
 
-module.exports = 
+}
+ehPositivo(6)
+ehPositivo(-10)
+
+ module.exports = 
 { 
   soma,
   multiplicacao,
